898b2d42a5e118403367e5153a5960a53d9b7b82
SehribanSevin <38330217+SehribanSevin@users.noreply.github.com>
2022-12-08 02:53:11 +0300

    Add files via upload

---
+++ UrunlerForm2.vue
@@ -0,0 +1,337 @@
+<template>
+  <div class="skMod">
+    <h3 class="skModulBslk" style="color: #ed2124">Yeni Ürünler</h3>
+    <a
+      href="https://www.evkur.com.tr/yeniurunler?banner=8limodulyeniurunler-27102021"
+      title="Tüm Ütü Modellerini Görün"
+      class="skTumKt"
+      style="color: #ed2124"
+      >Tümünü Gör →</a
+    >
+    <div class="skModulWrp krmz">
+      <div class="skModulElem">
+        <a
+          href="https://www.evkur.com.tr/oppo-a16-64gb-siyah-akilli-telefon?banner=8limodulyeniurunler-27102021"
+        >
+          <img
+            src="https://cdn.evkur.com.tr/c/Product/thumbs/oppo-a16-siyah-02_m4l2pb_250.jpg"
+            class="skModulRsm"
+            alt="Oppo A16 64GB Siyah Akıllı Telefon"
+          />
+          <h4 class="skModulIsm">Oppo A16 64GB<br />Akıllı Telefon</h4>
+          <div class="skModulFyt">3,199 TL</div>
+        </a>
+      </div>
+      <div class="skModulElem">
+        <a
+          href="https://www.evkur.com.tr/samsung/samsung-ue-43au8000uxtk-108-cm-crystal-4k-uhd-smart-hdr10-bluetooth-dahili-uydu-alicili-led-tv?banner=8limodulyeniurunler-27102021"
+        >
+          <img
+            src="https://cdn.evkur.com.tr/c/Product/thumbs/2_sf1y2j_1920_ae8b0q_250.jpg"
+            class="skModulRsm"
+            alt="Samsung UE-43AU8000UXTK 108 Cm Crystal 4K Uhd Smart Hdr10 Bluetooth Dahili Uydu Alıcılı Led Tv"
+          />
+          <h4 class="skModulIsm">
+            Samsung 108cm Crystal 4K Uhd Smart<br />Hdr10 Bluetooth Dahili Uydu Alıcılı
+            Led Tv
+          </h4>
+          <div class="skModulFyt">6,799 TL</div>
+        </a>
+      </div>
+      <div class="skModulElem">
+        <a
+          href="https://www.evkur.com.tr/realme-c25s-128gb-gri-akilli-telefon?banner=8limodulyeniurunler-27102021"
+        >
+          <img
+            src="https://cdn.evkur.com.tr/c/Product/thumbs/realme-c25s-gri-03_x6cgw4_250.jpg"
+            class="skModulRsm"
+            alt="Realme C25s 128GB Gri Akıllı Telefon"
+          />
+          <h4 class="skModulIsm">Realme C25s 128GB<br />Akıllı Telefon</h4>
+          <div class="skModulFyt">3,399 TL</div>
+        </a>
+      </div>
+      <div class="skModulElem">
+        <a
+          href="https://www.evkur.com.tr/3519-tac-ranf-nev-tak-martin-tk-v01-kirmizi-60251371?banner=8limodulyeniurunler-27102021"
+        >
+          <img
+            src="https://cdn.evkur.com.tr/c/Product/thumbs/tac-60251371_kz9p6r_250.jpg"
+            class="skModulRsm"
+            alt="Taç 3519 Ranforce Martin Tek Kişilik Kırmızı Nevresim Takımı 60251371"
+            style="border: solid 5px #fff"
+          />
+          <h4 class="skModulIsm">
+            Taç 3519 Ranforce Martin Tek Kişilik<br />Kırmızı Nevresim Takımı 60251371
+          </h4>
+          <div class="skModulFyt">329 TL</div>
+        </a>
+      </div>
+      <div class="skModulElem">
+        <a
+          href="https://www.evkur.com.tr/tefal-pure-max-fv9850e1-utu?banner=8limodulyeniurunler-27102021"
+        >
+          <img
+            src="https://cdn.evkur.com.tr/c/Product/thumbs/tefal-fv9850-pure-max-buharli-utu-1_x0p8xa_250.jpg"
+            class="skModulRsm"
+            alt="Tefal FV9850 Pure Max Buharlı Ütü 1830008490"
+          />
+          <h4 class="skModulIsm">Tefal FV9850 Pure Max<br />Buharlı Ütü</h4>
+          <div class="skModulFyt">1,169 TL</div>
+        </a>
+      </div>
+      <div class="skModulElem">
+        <a
+          href="https://www.evkur.com.tr/electrolux-wq71-p52ss-900277623-3in1--216v-dikey-sarjli-supurge?banner=8limodulyeniurunler-27102021"
+        >
+          <img
+            src="https://cdn.evkur.com.tr/c/Product/thumbs/electrolux-wq71-p52ss-dikey-sarjli-supurge_y6yzp8_250.jpg"
+            class="skModulRsm"
+            alt="Electrolux WQ71-P52SS Dikey Şarjlı Süpürge "
+          />
+          <h4 class="skModulIsm">Electrolux WQ71-P52SS<br />Dikey Şarjlı Süpürge</h4>
+          <div class="skModulFyt">2,999<sup>,90</sup> TL</div>
+        </a>
+      </div>
+      <div class="skModulElem">
+        <a
+          href="https://www.evkur.com.tr/arora-motor/arora-special-124-cc-euro-5-motosiklet-otv-dahil?banner=8limodulyeniurunler-27102021"
+        >
+          <img
+            src="https://cdn.evkur.com.tr/c/Product/thumbs/kirmizi2_s9g4hx_250.jpg"
+            class="skModulRsm"
+            alt="Arora Special 124 CC Euro 5 Motosiklet (ÖTV Dahil)SPECIAL-EURO5"
+          />
+          <h4 class="skModulIsm">
+            Arora Special 124 CC<br />Euro 5 Motosiklet (ÖTV Dahil)
+          </h4>
+          <div class="skModulFyt">21,900 TL</div>
+        </a>
+      </div>
+      <div class="skModulElem">
+        <a
+          href="https://www.evkur.com.tr/samsung-9-kg-1400d-a-sinif-addwash-kapak-eco-bubble-beyaz-cam-mak?banner=8limodulyeniurunler-27102021"
+        >
+          <img
+            src="https://cdn.evkur.com.tr/c/Product/thumbs/samsung-ww90t554daw-ah-a-sinifi-1400-devir-9-kg-camasir-makinesi-beyaz-1_j9tyk0_250.jpg"
+            class="skModulRsm"
+            alt="Samsung WW90T554DAW/AH A Sınıfı 1400 Devir 9 Kg Çamaşır Makinesi Beyaz"
+          />
+          <h4 class="skModulIsm">
+            Samsung A Sınıfı 1400<br />Devir 9 Kg Çamaşır Makinesi
+          </h4>
+          <div class="skModulFyt">6,009 TL</div>
+        </a>
+      </div>
+    </div>
+    <div class="skMod">
+      <h3 class="skModulBslk">Cep Telefonları</h3>
+      <a
+        href="https://www.evkur.com.tr/cep-telefonlari?banner=8limodulktceptelefonu-28102021"
+        title="Tüm Cep Telefonu Modellerini Görün"
+        class="skTumKt"
+        >Tümünü Gör →</a
+      >
+      <div class="skModulWrp mvi">
+        <div class="skModulElem">
+          <a
+            href="https://www.evkur.com.tr/samsung-galaxy-a12-128gb-mavi-akilli-telefon-a125f?banner=8limodulceptelefonu-28102021"
+          >
+            <img
+              src="https://cdn.evkur.com.tr/c/Product/thumbs/galaxy-a12-mavi_e9brk4_250.jpg"
+              class="skModulRsm"
+              alt="Samsung Galaxy A12 128GB Mavi Akıllı Telefon (A125F) SM-A125F-BLUE"
+            />
+            <h4 class="skModulIsm">
+              Samsung Galaxy A12 128GB<br />Akıllı Telefon (A125F)
+            </h4>
+            <div class="skModulFyt">2,899 TL</div>
+          </a>
+        </div>
+        <div class="skModulElem">
+          <a
+            href="https://www.evkur.com.tr/tecno-pova-ld7-128gb-mor-akilli-telefon?banner=8limodulceptelefonu-28102021"
+          >
+            <img
+              src="https://cdn.evkur.com.tr/c/Product/thumbs/tecno-pova-ld7-128gb-mor_pc93qw_250.jpg"
+              class="skModulRsm"
+              alt="Tecno Pova Ld7 128GB Mor Akıllı Telefon POVA-LD7-128GB-PURPLE"
+            />
+            <h4 class="skModulIsm">Tecno Pova Ld7 128GB<br />Akıllı Telefon</h4>
+            <div class="skModulFyt">2,849 TL</div>
+          </a>
+        </div>
+        <div class="skModulElem">
+          <a
+            href="https://www.evkur.com.tr/trident-a24-464gb-dualsim-mavi?banner=8limodulceptelefonu-28102021"
+          >
+            <img
+              src="https://cdn.evkur.com.tr/c/Product/thumbs/alcatel-a24_jm3r6n_r2y1bs_250.jpg"
+              class="skModulRsm"
+              alt="Trident A24 64GB Mavi Akıllı Telefon TR-A24-BLUE"
+            />
+            <h4 class="skModulIsm">Trident A24 64GB<br />Akıllı Telefon</h4>
+            <div class="skModulFyt">2,199 TL</div>
+          </a>
+        </div>
+        <div class="skModulElem">
+          <a
+            href="https://www.evkur.com.tr/oppo-a15s-64gb-siyah-akilli-telefon?banner=8limodulceptelefonu-28102021"
+          >
+            <img
+              src="https://cdn.evkur.com.tr/c/Product/thumbs/oppo-a15s-64gb-siyah-akilli-telefon_c3kea5_250.jpg"
+              class="skModulRsm"
+              alt="Oppo A15s 64GB Siyah Akıllı Telefon A15S-4/64GB-BLACK"
+            />
+            <h4 class="skModulIsm">Oppo A15s 64GB<br />Akıllı Telefon</h4>
+            <div class="skModulFyt">2,699 TL</div>
+          </a>
+        </div>
+        <div class="skModulElem">
+          <a
+            href="https://www.evkur.com.tr/samsung-galaxy-a02-32gb-siyah-akilli-telefon-a022f?banner=8limodulceptelefonu-28102021"
+          >
+            <img
+              src="https://cdn.evkur.com.tr/c/Product/thumbs/galaxy-a02-siayh_s2lxe5_250.jpg"
+              class="skModulRsm"
+              alt="Samsung Galaxy A02 32GB Siyah Akıllı Telefon (A022F) SM-A022F-BLACK"
+            />
+            <h4 class="skModulIsm">
+              Samsung Galaxy A02 32GB<br />Akıllı Telefon (A022F)
+            </h4>
+            <div class="skModulFyt">2,099 TL</div>
+          </a>
+        </div>
+        <div class="skModulElem">
+          <a
+            href="https://www.evkur.com.tr/hiking-a26-128gb-siyah-akilli-telefon?banner=8limodulceptelefonu-28102021"
+          >
+            <img
+              src="https://cdn.evkur.com.tr/c/Product/thumbs/hiking-a26-siyah-akilli-telefon-03_x1gpw7_250.jpg"
+              class="skModulRsm"
+              alt="Hiking A26 128GB Siyah Akıllı Telefon HK-A26-6/128GB-BLACK"
+            />
+            <h4 class="skModulIsm">Hiking A26 128GB<br />Akıllı Telefon</h4>
+            <div class="skModulFyt">2,799 TL</div>
+          </a>
+        </div>
+        <div class="skModulElem">
+          <a
+            href="https://www.evkur.com.tr/oppo-a16-64gb-mavi-akilli-telefon?banner=8limodulceptelefonu-28102021"
+          >
+            <img
+              src="https://cdn.evkur.com.tr/c/Product/thumbs/oppo-a16-mavi_km8b1x_250.jpg"
+              class="skModulRsm"
+              alt="Oppo A16 64GB Mavi Akıllı Telefon A16-4/64GB-BLUE"
+            />
+            <h4 class="skModulIsm">Oppo A16 64GB<br />Akıllı Telefon</h4>
+            <div class="skModulFyt">3,199 TL</div>
+          </a>
+        </div>
+        <div class="skModulElem">
+          <a
+            href="https://www.evkur.com.tr/trident-a23-max-64gb-yildiz-siyahi-akilli-telefon?banner=8limodulceptelefonu-28102021"
+          >
+            <img
+              src="https://cdn.evkur.com.tr/c/Product/thumbs/oppo-a74-128gb-mavi-akilli-telefon_w1ezd2_250.jpg"
+              class="skModulRsm"
+              alt="Trident A23 Max 64GB Yıldız Siyahı Akıllı Telefon TR-A23 MAX-STARLIGHT BLACK"
+            />
+            <h4 class="skModulIsm">Trident A23 Max 64GB<br />Akıllı Telefon</h4>
+            <div class="skModulFyt">1,999 TL</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.skModulWrp {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 5%;
+  margin-top: 0.5%;
+}
+.skModulElem {
+  border: 1px solid grey;
+  border-radius: 5px;
+  padding: 5px;
+  margin-top: 20px;
+}
+.skModulIsm {
+  color: #707070 !important;
+  font-size: 0.16rem;
+  font-family: "Proxima";
+  text-align: center;
+  font-weight: normal;
+  line-height: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  line-clamp: 2;
+  -webkit-box-orient: vertical;
+  margin-bottom: 3%;
+  scrollbar-width: none;
+}
+.skModulFyt {
+  font-size: 0.2rem;
+  font-family: "Proxima";
+  color: #ed2124;
+  text-align: center;
+  font-weight: bold;
+}
+.skModulElem a {
+  text-decoration: none;
+}
+.skModulRsm {
+  width: 82%;
+  height: auto;
+  display: block;
+  margin: auto;
+}
+.skModulBslk {
+  font-size: 0.2rem;
+  font-family: "Proxima";
+  color: #555;
+  width: auto;
+  float: left;
+  margin-bottom: 0.08rem;
+}
+.skMod {
+  margin: 3% auto;
+  padding: 0 0.2rem;
+  max-width: 1200px;
+}
+.skModulWrp {
+  border-top: solid 3px #ed2124;
+  margin-top: 0.5%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 100%;
+  padding: 0 0 1% 0;
+}
+.skTumKt {
+  font-size: 0.16rem !important;
+  float: right;
+  font-family: "Proxima";
+  font-weight: bold;
+  text-decoration: none;
+  color: #555;
+  clear: right;
+  width: auto;
+  height: 0.26rem;
+  line-height: 0.36rem;
+}
+.mvi {
+  border-top: solid 3px #1b7bdb;
+}
+</style>
