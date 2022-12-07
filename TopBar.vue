c99a690038f78800d1374e75381506c55642539d
SehribanSevin <38330217+SehribanSevin@users.noreply.github.com>
2022-12-08 02:51:54 +0300

    Add files via upload

---
+++ TopBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="background: rgb(154, 0, 0)">
+    <div class="">
+      <div class="sp-custom-132-1">
+        <a
+          style="display: inline-block"
+          href="https://www.evkur.com.tr/arti-10-taksit-kampanyasi-kosullari"
+        >
+          <img
+            src="https://image.useinsider.com/evkur/defaultImageLibrary/unnamed-1609831068.jpeg"
+          />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+element.style {
+  background: rgb(154, 0, 0);
+}
+header {
+  padding: 0;
+  margin: 0;
+  font-size: 0;
+  outline: none;
+}
+div {
+  display: block;
+}
+</style>
