3d5b04d8f76c32d41f132dadb50858e35ebd8a60
mervear <104269519+mervear@users.noreply.github.com>
2022-12-08 02:37:56 +0300

    Add files via upload

---
+++ TaksitOdemesi.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sloganKalip">
+    <a href="https://www.evkur.com.tr/odeme" class="sloganLink" target="_blank">
+      <img
+        src="https://cdn.evkur.com.tr/Content/contentimg/ozelsf/evkurtaksit-odeme-buyuk.png"
+        class="aktfBnr"
+        id="rsm1"
+        alt="Taksitlerinizi Kolayca Ödeyin"
+      />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
